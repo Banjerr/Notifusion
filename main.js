547279398de953ef35079e7349da9c9a227c278c
Ben Redden <benjamminredden@gmail.com>
2016-04-30 14:26:19 -0400

    getting request_token object back

--- main.js
+++ main.js
@@ -15,12 +15,12 @@ let mainWindow;
 var options =
 {
     client_id: 'zjdmn6jek8xd5v6yz9eptc8m',
-    redirect_uri: "http://localhost/notifusion/index.html",
-    response_type: 'code'
+    redirect_uri: "https://localhost/notifusion/index.html",
+    response_type: 'code',
+    client_secret: 'ReFbmkmPWq'
 };
 var isUrl = 'https://signin.infusionsoft.com/app/oauth/authorize?';
 var authUrl = isUrl + 'client_id=' + options.client_id + '&redirect_uri=' + options.redirect_uri + '&response_type=' + options.response_type;
-
 
 function createWindow ()
 {
@@ -34,10 +34,12 @@ function createWindow ()
         }
     });
 
+    // require jQuery
     mainWindow.$ = require('jQuery');
     mainWindow.jQuery = require('jQuery');
 
     // and load the index.html of the app.
+    // mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.loadURL(authUrl);
 
     // Open the DevTools.
@@ -58,13 +60,14 @@ function createWindow ()
 
       if (code || error) {
           // Close the browser if code found or error
-          console.log('code or error')
+          console.log('code or error');
           //authWindow.destroy();
       }
 
       // If there is a code, proceed to get token from IS
       if (code) {
           console.log(code);
+          console.log(options.client_id);
           console.log('get an access token, ya big dummy!');
 
         // send a post to request the access_token
@@ -72,11 +75,10 @@ function createWindow ()
             'https://api.infusionsoft.com/token',
             { form: {
                     client_id: options.client_id,
-                    client_secret: options.redirect_uri,
+                    client_secret: options.client_secret,
                     code: code,
                     grant_type: 'authorization_code',
-                    //r/edirect_uri: 'http://localhost/notifusion/index.html'
-                    redirect_uri: 'http://' + __dirname + '/index.html'
+                    redirect_uri: options.redirect_uri
                 }
             },
             function (error, response, body)
@@ -89,7 +91,9 @@ function createWindow ()
                 if (error)
                 {
                     console.log('somethin goofed');
+                    console.log(error);
                 }
+
             }
         );
 
@@ -106,9 +110,9 @@ function createWindow ()
       handleCallback(url);
     });
 
-    mainWindow.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl)
+    mainWindow.webContents.on('did-get-redirect-request', function (event, url)
     {
-      handleCallback(newUrl);
+      handleCallback(url);
     });
 
     // Emitted when the window is closed.
@@ -149,3 +153,113 @@ app.on('activate', function ()
       mainWindow = null;
   }, false);
 });
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // oAuth stuff \\
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\
+//
+// function oAuthTokenRequest()
+// {
+//     // make the auth window
+//     authWindow = new BrowserWindow({
+//         width: 960,
+//         height: 960,
+//         'webPreferences': {
+//           'nodeIntegration': false,
+//           'webSecurity': false
+//         }
+//     });
+//
+//     // oAuth stuff
+//     var options =
+//     {
+//         client_id: 'zjdmn6jek8xd5v6yz9eptc8m',
+//         redirect_uri: "https://localhost/notifusion/index.html",
+//         response_type: 'code',
+//         client_secret: 'ReFbmkmPWq'
+//     };
+//     var isUrl = 'https://signin.infusionsoft.com/app/oauth/authorize?';
+//     var authUrl = isUrl + 'client_id=' + options.client_id + '&redirect_uri=' + options.redirect_uri + '&response_type=' + options.response_type;
+//
+//     // listen for the access token
+//     authWindow.webContents.on('did-get-redirect-request', function (event, arg)
+//     {
+//       console.log(event);
+//       console.log(arg);
+//     });
+//
+//     authWindow.webContents.on('will-navigate', function (event, url)
+//     {
+//       handleCallback(url);
+//     });
+//
+//     authWindow.webContents.on('did-get-redirect-request', function (event, url)
+//     {
+//       handleCallback(url);
+//     });
+// }
+//
+// function handleCallback (url)
+// {
+//   var raw_code = /code=([^&]*)/.exec(url) || null;
+//   var code = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
+//   var error = /\?error=(.+)$/.exec(url);
+//
+//   if (code || error) {
+//       // Close the browser if code found or error
+//       console.log('code or error');
+//       authWindow.destroy();
+//   }
+//
+//   // If there is a code, proceed to get token from IS
+//   if (code) {
+//       console.log(code);
+//       console.log(options.client_id);
+//       console.log('get an access token, ya big dummy!');
+//
+//     // send a post to request the access_token
+//     request.post(
+//         'https://api.infusionsoft.com/token',
+//         { form: {
+//                 client_id: options.client_id,
+//                 client_secret: options.client_secret,
+//                 code: code,
+//                 grant_type: 'authorization_code',
+//                 redirect_uri: options.redirect_uri
+//             }
+//         },
+//         function (error, response, body)
+//         {
+//             if (!error && response.statusCode == 200)
+//             {
+//                 console.log(body)
+//             }
+//
+//             if (error)
+//             {
+//                 console.log('somethin goofed');
+//                 console.log(error);
+//             }
+//
+//         }
+//     );
+//
+//   }
+//   else if (error)
+//   {
+//       alert('Oops! Something went wrong and we couldn\'t' +
+//       'log you into Infusionsoft. Please try again.');
+//   }
+// }
+//
+// // function to grab query vars
+// function getQueryVariable(variable)
+// {
+//    var query = window.location.search.substring(1);
+//    var vars = query.split("&");
+//    for (var i=0;i<vars.length;i++) {
+//            var pair = vars[i].split("=");
+//            if(pair[0] == variable){return pair[1];}
+//    }
+//    return(false);
+// }
